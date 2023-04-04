d64ae367d3bb9f58300112facffd7b4e9f00344f
ikramzaidan <ikramzaidann@gmail.com>
2023-04-05 00:45:56 +0700

    Create search feature #11

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,11 @@ class Product extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function($query, $search){
+            return $query->where('name', 'like', '%' . $search . '%');
+        });
+    }
 }
--- resources/views/web/find.blade.php
+++ resources/views/web/find.blade.php
@@ -29,19 +29,6 @@
         
         @include('web.partials.navbar')
 
-        <!-- Masthead-->
-        <header class="masthead text-white">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-8">
-                        <h1>NoWaste.</h1>
-                        <h1>stok makanan berlebih</h1>
-                        <h1>bisa berarti untuk orang lain</h1>
-                    </div>
-                </div>
-            </div>
-        </header>
-
         <!-- Marketplace-->
         <section class="pt-5 pb-3">
             <div class="container">
@@ -52,9 +39,7 @@
                             <div id="search-icon" class="align-self-center me-3 pt-1 text-secondary">
                                 <i class="fa fa-search"></i>
                             </div>
-                            <form action="/">
-                                <input class="search-input w-100 pt-1" type="search" name="search" placeholder="Cari merchant atau makanan" value="{{ request('search') }}" aria-label="Search">
-                            </form>
+                            <input class="search-input w-100 pt-1" type="search" placeholder="Cari merchant atau makanan" aria-label="Search">
                         </div>
                     </form>
                 </div>
@@ -63,104 +48,70 @@
 
         <section class="bg-white pt-5 pb-3">
             <div class="container">
-                <h2 class="mb-4"><span class="text-primary">Favorit</span> orang-orang</h2>
-                <div class="row mb-4">
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                </div>
-                <a class="btn btn-outline-primary w-100 py-2">Lihat semua</a>
-            </div>
-        </section>
-
-        <section class="bg-white pt-5 pb-3">
-            <div class="container">
-                <h2 class="mb-4">Ada yang <span class="text-primary">Gratis</span> nih!</h2>
-                <div class="row mb-4">
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card border-0">
-                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
-                            <div class="text-secondary">Roti, Kue</div>
-                        </div>
-                    </div>
-                </div>
-                <a class="btn btn-outline-primary w-100 py-2">Lihat semua</a>
-            </div>
-        </section>
-
-        <section class="bg-white py-5 mb-3">
-            <div class="container">
-                <h2 class="mb-4">Cari tahu di <span class="text-primary">Artikel</span></h2>
+                <h2 class="mb-4"><span class="text-primary">'{{ $keyword }}'</span> di Merchant</h2>
                 <div class="row">
-                    <div class="col-lg-6">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <img class="img-thumbnail rounded-4 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            </div>
-                            <div class="col-lg-6">
-                                <a class="h5 text-dark text-decoration-none">Bagi-bagi Makanan Gratis, Rayakan Bulan Kebersamaan</a>
-                                <div class="text-primary">23 Mar 2023</div>
-                                <p class="font-maisonBook" style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quae fuga corrupti assumenda quaerat necessitatibus sunt distinctio sed sequi dolorem, accusamus quos repellendus quod dolore eaque dolores rerum. Reiciendis, reprehenderit.</p>
-                            </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <img class="img-thumbnail rounded-4 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
-                            </div>
-                            <div class="col-lg-6">
-                                <a class="h5 text-dark text-decoration-none">Pakai Debit BCA, Harga Lebih Murah</a>
-                                <div class="text-primary">6 Mar 2023</div>
-                                <p class="font-maisonBook" style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quae fuga corrupti assumenda quaerat necessitatibus sunt distinctio sed sequi dolorem, accusamus quos repellendus quod dolore eaque dolores rerum. Reiciendis, reprehenderit.</p>
-                            </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="bg-white pt-3 pb-5">
+            <div class="container">
+                <h2 class="mb-4"><span class="text-primary">'{{ $keyword }}'</span> di Produk</h2>
+                <div class="row mb-3">
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border-0">
+                            <img class="img-thumbnail rounded-3 border-0 p-0 mb-3" src="../assets/img/bg-masthead.jpg">
+                            <a class="h5 text-dark text-decoration-none text-truncate">Dunkin Donut Jatinangor</a>
+                            <div class="text-secondary">Roti, Kue</div>
                         </div>
                     </div>
                 </div>
