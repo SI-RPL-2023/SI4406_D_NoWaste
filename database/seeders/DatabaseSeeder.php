9ec4c7e20e5b6f090db862661a0714395fa51d17
ikramzaidan <ikramzaidann@gmail.com>
2023-04-05 03:09:02 +0700

    Add bio and photo column.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Merchant::factory()->create([
+             'name' => 'Dunkin Donut Bojongsoang',
+             'email' => 'test@example.com',
+             'password' => 'password',
+        ]);
     }
 }
