f21864888566a31d12dc5437e8e98fd26d6425d3
ikramzaidan <ikramzaidann@gmail.com>
2023-05-09 22:52:22 +0700

    create filtering search feature

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,23 @@ class Product extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function scopeFilter($query)
+    {
+        if(request('keyword') && request('free') == NULL && request('minPrice') == NULL && request('maxPrice') == NULL){
+            return $query->where('name', 'like', '%' . request('keyword') . '%');
+        }
+        elseif(request('keyword') && request('free') == 'yes'){
+            return $query->where('name', 'like', '%' . request('keyword') . '%')->where('price', '=', 0);
+        }
+        elseif(request('keyword') && request('minPrice') && request('maxPrice') == NULL){
+            return $query->where('name', 'like', '%' . request('keyword') . '%')->where('price', '>=', request('minPrice'));
+        }
+        elseif(request('keyword') && request('maxPrice') && request('minPrice') == NULL){
+            return $query->where('name', 'like', '%' . request('keyword') . '%')->where('price', '<=', request('maxPrice'));
+        }
+        elseif(request('keyword') && request('minPrice') != NULL && request('maxPrice') != NULL){
+            return $query->where('name', 'like', '%' . request('keyword') . '%')->whereBetween('price', [request('minPrice'), request('maxPrice')]);
+        }
+    }
 }
