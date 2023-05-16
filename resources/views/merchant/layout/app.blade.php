cd2b75a9091d5ebe9f6332dcd4cc506dfe4375c4
ikramzaidan <ikramzaidann@gmail.com>
2023-05-16 16:16:59 +0700

    edit merchant portal links

--- resources/views/merchant/layout/app.blade.php
+++ resources/views/merchant/layout/app.blade.php
@@ -26,7 +26,7 @@
             <button class="btn btn-light d-lg-none me-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand font-maisonExtraBold" href="/merchant">NoWaste</a>
+            <a class="navbar-brand font-maisonExtraBold" href="#">NoWaste</a>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="/merchant/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -49,7 +49,7 @@
             <div class="col-lg-3 col-xl-2 pt-3 pe-0 border-end">
                 <div class="offcanvas-lg offcanvas-start vh-100" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasResponsiveLabel">NoWaste</h5>
+                        <h5 class="offcanvas-title" id="offcanvasResponsiveLabel">SekolahApp</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasNavbar" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body">
@@ -65,6 +65,16 @@
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="/merchant/profile">Profil</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu w-100">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
                             </li>
                         </ul>
                     </div>
