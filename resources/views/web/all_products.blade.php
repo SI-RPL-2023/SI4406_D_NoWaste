f1ebec730854fa2a43b38721edb2a6f20d808440
ikramzaidan <ikramzaidann@gmail.com>
2023-06-05 13:24:53 +0700

    Membuat halaman baru untuk menampilkan semua merchant dan produk.

--- resources/views/web/all_products.blade.php
+++ resources/views/web/all_products.blade.php
@@ -29,46 +29,39 @@
         
         @include('web.partials.navbar')
 
-        <header>
-            <div class="container py-5">
-                <nav aria-label="breadcrumb" class="mb-3">
+        <!-- Search Bar-->
+        <section class="py-5">
+            <div class="container">
+                <h2 class="text-center mb-4">Temukan <span class="text-primary">Makanan</span>mu</h2>
+                <div class="d-flex justify-content-center">
+                    <div id="searchbar" class="w-lg-50 form-control px-5 py-3 rounded-pill fs-5 d-flex">
+                        <div id="search-icon" class="align-self-center me-3 pt-1 text-secondary">
+                            <i class="fa fa-search"></i>
+                        </div>
+                        <form class="w-100" action="/search">
+                            <input class="search-input w-100 pt-1" type="search" name="keyword" placeholder="Cari merchant atau makanan" value="{{ request('keyword') }}" aria-label="Search">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <h2 class="mb-1">Semua <span class="text-primary">Produk</span></h2>
+                <nav class="mb-4" aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item"><a href="/merchants">Merchant</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">{{ $Merchant->name }}</li>
+                        <li class="breadcrumb-item active" aria-current="page">Semua Produk</li>
                     </ol>
                 </nav>
-                <div class="card bg-primary rounded-3">
-                    <div class="card-body text-white pt-5">
-                        <div class="row">
-                            <div class="col-lg-9 align-self-end">
-                                <h1 class="font-maisonExtraBold mb-2">
-                                    {{ $Merchant->name }}
-                                    @if($Merchant->status == 1)<i class="fs-3 fa-solid fa-circle-check text-info" title="Merchant Terverifikasi"></i>@endif
-                                </h1>
-                                <div>{{ $Merchant->bio }}</div>
-                                @if(isset($Merchant->map))<a href="#location" class="text-white font-maisonBook">Lihat Lokasi</a>@endif
-                            </div>
-                            <div class="col-lg-3 d-flex justify-content-end">
-                                <div class="img-profile">
-                                    <img src="{{ $Merchant->photo != null ? '/storage/'.$Merchant->photo : '/assets/img/no-image.png' }}" class="rounded-3">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-            </div>
-        </header>
-
-        <section class="{{ isset($Merchant->map) ? 'mb-3' : 'mb-5' }}">
-            <div class="container">
-                <div class="row">
+                <div class="row mb-2 mb-lg-4">
                     @if (count($Products) != 0)
                         @foreach ($Products as $Product)
-                        <div class="col-lg-3 col-md-4 mb-3">
+                        <div class="col-md-6 col-lg-3 mb-3">
                             <div class="card border-0">
                                 <img class="img-product rounded-3 border-0 p-0 mb-2 mb-lg-3" src="../storage/{{ $Product->photo }}">
-                                <a class="h5 text-dark text-decoration-none text-truncate mb-0">{{ $Product->name }}</a>
+                                <a href="/merchants/{{ $Product->merchant->id }}" class="h5 text-dark text-decoration-none text-truncate mb-0">{{ $Product->name }}</a>
                                 <div>
                                     @if ($Product->price != 0)
                                     <span class="text-secondary">IDR {{ number_format($Product->price) }}</span>
@@ -76,27 +69,15 @@
                                     <span class="text-primary">Gratis</span>
                                     @endif
                                 </div>
-                                <div class="font-maisonDemi">
-                                    Sisa {{ $Product->stock }}
-                                </div>
                             </div>
                         </div>
                         @endforeach
                     @else
-                    <div class="h3 py-5 text-center">Oops, belum ada Produk yang bisa kami temukan!</div>
+                    <div class="h3 py-5 text-center">Oops, belum ada Produk yang tersedia disini!</div>
                     @endif
                 </div>
             </div>
         </section>
-        
-        @if(isset($Merchant->map))
-        <section id="location" class="mb-5">
-            <div class="container">
-                <h3>Lokasi</h3>
-                {!! $Merchant->map !!}
-            </div>
-        </section>
-        @endif
         
         @include('web.partials.footer')
 
